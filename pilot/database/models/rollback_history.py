4076dbdbc89c2314def9e7dc4f61be7e1684a896
bc-rep-project <156528154+bc-rep-project@users.noreply.github.com>
2024-05-06 05:22:28 +0000

    07:21_6May24

--- pilot/database/models/rollback_history.py
+++ pilot/database/models/rollback_history.py
@@ -1,7 +1,20 @@
-from peewee import ForeignKeyField, UUIDField, DateTimeField, JSONField
+# from peewee import ForeignKeyField, UUIDField, DateTimeField, JSONField
+# from database.models.components.base_models import BaseModel
+# from database.models.app import App
+# from database.models.components.sqlite_middlewares import JSONField
+
+
+# class RollbackHistory(BaseModel):
+#     app = ForeignKeyField(App, on_delete='CASCADE')
+#     step_id = UUIDField()
+#     timestamp = DateTimeField(default=datetime.now)
+#     step_data = JSONField()
+
+from peewee import ForeignKeyField, UUIDField, DateTimeField
 from database.models.components.base_models import BaseModel
 from database.models.app import App
 from database.models.components.sqlite_middlewares import JSONField
+from datetime import datetime
 
 
 class RollbackHistory(BaseModel):
